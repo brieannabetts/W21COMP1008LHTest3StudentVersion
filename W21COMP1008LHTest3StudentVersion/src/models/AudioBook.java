ef19bedfbec62ce6af4c64542d5bb5e8173b31bd
brieannabetts <bgbetts@lakeheadu.ca>
2021-04-21 09:25:41 -0500

    Initial commit

---
+++ W21COMP1008LHTest3StudentVersion/src/models/AudioBook.java
@@ -0,0 +1,17 @@
+//Brieanna Betts
+//1158732
+package models;
+
+public class AudioBook  {
+    private int lengthInSec;
+
+    public AudioBook(String bookName, Person author, double price, int lengthInSec) {
+    }
+
+    public int getLengthInSec() {
+        return lengthInSec;
+    }
+
+    public void setLengthInSec(int lengthInSec) {
+    }
+}
